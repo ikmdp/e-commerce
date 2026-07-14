fdab202e36ac256ae477c169bba02981c4697b67
katherine moraes de paula <katherinemoraesdepaula@gmail.com>
2026-07-14 19:00:05 -0300

    feat(app):importando o arquivo produto do arquivo produto.ts para ser usado no componente app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+//import { RouterOutlet } from '@angular/router';//remove a importaçao de outlet para esse componente pois nao é necessario para esse componente
+import {Produto} from './components/produto/produto';//importando o arquivo produto do arquivo produto.ts para ser usado no componente app
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Produto],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
