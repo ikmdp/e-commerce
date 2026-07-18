813b113b5f2d417a841a4394c7f2c230a69f6d2d
katherine moraes de paula <katherinemoraesdepaula@gmail.com>
2026-07-17 21:29:54 -0300

    feat: adiciona nova pasta ListaProdutos

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 //import { RouterOutlet } from '@angular/router';//remove a importaçao de outlet para esse componente pois nao é necessario para esse componente
-import {Produto} from './components/produto/produto';//importando o arquivo produto do arquivo produto.ts para ser usado no componente app
+//import {Produto} from './components/produto/produto';//importando o arquivo produto do arquivo produto.ts para ser usado no componente app
+import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
 @Component({
   selector: 'app-root',
-  imports: [Produto],
+  imports: [ListaProdutos],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
