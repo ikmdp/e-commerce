459532a8b98fa70d5192a6b0388955aa8a2c2170
katherine moraes de paula <katherinemoraesdepaula@gmail.com>
2026-07-17 21:32:52 -0300

    feat: adicionar substituição da lista e monitoramento com signals

--- src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -3,9 +3,11 @@ import { Produto } from '../produto/produto';
 import { signal } from '@angular/core'; //!importe do signal na pasta lista-produto.ts
 import { computed } from '@angular/core';
 import { PrecoFormatadoPipe } from '../../../shared/pipes/preco-formatado-pipe';
+import { effect } from '@angular/core';
+import { UpperCasePipe } from '@angular/common';
 @Component({
   selector: 'app-lista-produtos',
-  imports: [Produto, PrecoFormatadoPipe],
+  imports: [Produto, PrecoFormatadoPipe, UpperCasePipe],
   templateUrl: './lista-produtos.html',
   styleUrl: './lista-produtos.css',
 })
@@ -20,6 +22,7 @@ export class ListaProdutos {
   //!funçao para exibir produtos selecionados pelo usuario no console
   exibirProduto(nome: string){
     console.log('Produto Selecionado: ', nome);
+    this.produtoSelecionado.set(nome);
   }
   adicionarProduto(){
     //!funçao que adiciona produto usando metodo update
@@ -34,6 +37,30 @@ export class ListaProdutos {
   valorTotal = computed(() =>
   {return this.produtos().reduce((total, item) =>
   total + item.preco,0
-  )}
-  );
+  )});
+  //!funçao que substitui a lista atual usando o metodo set()
+  substituirProdutos(){
+    this.produtos.set([
+      { nome:'Teclado', preco: 50 },
+      { nome: 'Mouse', preco: 15 },
+      { nome: 'Monitor', preco: 500 },
+      { nome: 'Desktop', preco: 1500 },
+      { nome: 'Headset', preco: 30},
+    ]);
+  }
+  //!metodo para monitorar alteraçoes em tempo real usando metodo effect.
+  constructor() {
+    effect(() =>{
+      console.log('Lista de Produtos Alterados: ', this.produtos());
+    });
+     effect(() =>{
+      console.log('Valor atualizado: ', this.valorTotal());
+     });
+      effect(() =>{
+        if(typeof document !== 'undefined'){
+          document.title = `(${this.totalProdutos()})- Loja`;
+        }
+      });
+    }
+    produtoSelecionado = signal <string | null> (null);
 }
